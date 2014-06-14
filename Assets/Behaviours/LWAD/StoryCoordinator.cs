27dec9d635a61cdc9692096b9bb66da4c8c775de
TomMulvaney <mulvaney.tom@gmail.com>
2014-06-14 14:15:04 +0100

    Added member variables to StoryCoordinator.

--- Assets/Behaviours/LWAD/StoryCoordinator.cs
+++ Assets/Behaviours/LWAD/StoryCoordinator.cs
@@ -3,6 +3,23 @@ using System.Collections;
 
 public class StoryCoordinator : Singleton<StoryCoordinator>
 {
+	[SerializeField]
+	private UIPanel m_imagePanel;
+	[SerializeField]
+	private UITexture m_foreground;
+	[SerializeField]
+	private UITexture m_midground;
+	[SerializeField]
+	private UITexture m_background;
+	[SerializeField]
+	private UIPanel m_textPanel;
+	[SerializeField]
+	private UILabel m_texLabel;
+	[SerializeField]
+	private UIPanel m_buttonPanel;
+	[SerializeField]
+	private AudioSource m_audioSource;
+
 	int m_currentPage = 0;
 
 	void Start()
